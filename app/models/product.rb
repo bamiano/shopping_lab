d058791b68593a8b998e2fc462c9a2ccd2321000
bamiano <bamiano@email.arizona.edu>
2014-11-13 06:20:49 -0800

    associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
-
+	validates :name, presence: true
+	validates :category, presence: true
+	validates :price, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create({username: "testing", password: "secret", is_admin: true})
+User.create({username: "admin", password: "secret", is_admin: true})
 User.create({username: "user1", password: "testing"})
 User.create({username: "user2", password: "testing"})
 User.create({username: "user3", password: "testing"})
